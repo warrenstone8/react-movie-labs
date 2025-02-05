376f654e59661cb11f3c8104fe50cc379d42333a
warren <20101336@mail.wit.ie>
2025-02-05 13:28:11 +0000

    Movie card component created

--- movies/src/components/moviecard/main.jsx
+++ movies/src/components/moviecard/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import HomePage from "./pages/homePage";
+import HomePage from "../../pages/homePage";
 
 const sample = {
   adult: false,
