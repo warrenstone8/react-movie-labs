31582491403d1088c892d4471578d9edb167bdda
warren <20101336@mail.wit.ie>
2025-02-12 10:51:57 +0000

    Movie Details coming from API.

--- movies/src/components/moviecard/index.jsx
+++ movies/src/components/moviecard/index.jsx
@@ -12,6 +12,8 @@ import StarRateIcon from "@mui/icons-material/StarRate";
 import IconButton from "@mui/material/IconButton";
 import Grid from "@mui/material/Grid2";
 import img from '../../images/film-poster-placeholder.png'
+import { Link } from "react-router";
+
 
 export default function MovieCard(props) {
   const movie = props.movie;
@@ -47,7 +49,12 @@ export default function MovieCard(props) {
           <FavoriteIcon color="primary" fontSize="large" />
         </IconButton>
         <Button variant="outlined" size="medium" color="primary">
-          More Info ...
+        <Link to={`/movies/${movie.id}`}>
+          <Button variant="outlined" size="medium" color="primary">
+            More Info ...
+          </Button>
+        </Link>
+
         </Button>
       </CardActions>
     </Card>
--- movies/src/pages/movieDetailspage.jsx
+++ movies/src/pages/movieDetailspage.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+
 import MovieHeader from "../components/headerMovie";
 import MovieDetails from "../components/movieDetails";
 import Grid from "@mui/material/Grid2";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
+import React, {useState, useEffect}  from "react";
+import { useParams } from 'react-router';
+
 
 const MoviePage = (props) => {
-  const movie = props.movie;
-  const images = props.images;
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${import.meta.env.VITE_TMDB_KEY}`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((movie) => {
+        // console.log(movie)
+        setMovie(movie);
+      });
+  }, [id]);
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => json.posters)
+      .then((images) => {
+        // console.log(images)
+        setImages(images);
+      });
+      // eslint-disable-next-line
+  }, []);
+
 
   return (
     <>
@@ -21,24 +52,25 @@ const MoviePage = (props) => {
                 flexWrap: "wrap",
                 justifyContent: "space-around",
               }}>
-                <ImageList
-                  sx={{
-                    height: "100vh",
-                  }}
-                  cols={1}
-                >
-                  {images.map((image) => (
-                    <ImageListItem
-                      key={image}
-                      cols={1}
-                    >
-                      <img
-                        src={`https://image.tmdb.org/t/p/w500/${image}`}
-                        alt={image}
-                      />
-                    </ImageListItem>
-                  ))}
-                </ImageList>
+                 <ImageList
+    sx={{
+      height: "100vh",
+    }}
+    cols={1}
+  >
+    {images.map((image) => (
+      <ImageListItem
+      key={image.file_path}
+      cols={1}
+      >
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+          alt={image.file_path}
+        />
+      </ImageListItem>
+    ))}
+  </ImageList>
+
               </div>
             </Grid>
             <Grid size={{xs: 9}}>
